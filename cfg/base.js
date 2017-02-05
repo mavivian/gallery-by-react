c82d753e2bd78fe1fc578846936c693b945a6944
vivian <majunvivian@163.com>
2017-02-05 18:21:25 +0800

    xx

--- cfg/base.js
+++ cfg/base.js
@@ -15,9 +15,9 @@ module.exports = {
   debug: true,
   devtool: 'eval',
   output: {
-    path: path.join(__dirname, '/dist/assets'),
+    path: path.join(__dirname, '/../dist/assets'),
     filename: 'app.js',
-    publicPath: defaultSettings.publicPath
+    publicPath: 'assets/'
   },
   devServer: {
     contentBase: './src/',
